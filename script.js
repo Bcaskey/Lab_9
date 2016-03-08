001be9ead0aaf52ec20b1830ee7563bf64d4294f
Bcaskey <bcaskeyu@charter.net>
2016-03-07 20:05:30 -0600

    Posting, for review. Lost styles from Vehicle

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 
+// Super Class start ---------------------------------------
 var Vehicle = function() {
     this.value = null;
     this.div = null;
 }
 
 Vehicle.prototype.insert = function() {
-    console.log('inside vehicle insert');
+    //console.log('inside vehicle insert');
     this.div = document.createElement('div');
     this.div.className = 'myVehicle';
     this.div.style.backgroundColor = 'red';
-    this.div.style.width = '140px';
-    this.div.style.height = '100px';
+    this.div.style.width = '120px';
+    this.div.style.height = '80px';
     this.div.style.position = 'absolute';
     // this.div.style.left = '200px';
     // this.div.style.top = '200px';
@@ -19,13 +20,9 @@ Vehicle.prototype.insert = function() {
     this.div.style.left = Math.floor(Math.random() * (ww - 140)) + 'px';
     this.div.style.top = Math.floor(Math.random() * (wh - 100)) + 'px';
     console.log(Math.floor(Math.random() * ww) - 140);
-    //this.style.left = ;
-    //this.style.top = ;
     this.move;
+    this.moveback;
     document.getElementById('gameArea').appendChild(this.div);
-}
-
-Vehicle.prototype.move = function() {
 }
 
 Vehicle.prototype.move = function() {
@@ -35,11 +32,75 @@ Vehicle.prototype.move = function() {
     this.div.animate({left: ww + 'px'}, 5000);
 }
 
+Vehicle.prototype.moveback = function() {
+    this.div = $('.myVehicle');
+    // this.div = document.getElementsByClassName('myVehicle');
+    var ww = window.innerWidth;
+    this.div.animate({left: '10px'}, 5000);
+}
+
+// Super Clas Stop ------------------------------------------
+
+// Child Classes Start --------------------------------------
+var Car = function () {
+    //Die.call(this, 'Hello');
+    //this.value = num;
+    console.log('inside car');
+}
+
+Car.prototype = Object.create(Vehicle.prototype);
+Car.prototype.constructor = Car; //This is the function that calls car
+Car.prototype.insert = function() {
+    this.div = document.createElement('div');
+    this.div.className = 'myCar';
+    //this.div.style.backgroundColor = 'red'; --Moved to CSS
+}
+Car.prototype.move = function() {
+    this.div = $('.myVehicle');
+    var ww = window.innerWidth;
+    this.div.animate({left: ww + 'px'}, 5000); //Same as Vehicle speed
+}
+
+var CopCar = function () {
+    //Die.call(this, 'Hello');
+    //this.value = num;
+    console.log('inside cop car');
+}
+
+CopCar.prototype = Object.create(Car.prototype);
+CopCar.prototype.constructor = CopCar;
+CopCar.prototype.insert = function() {
+    this.div = document.createElement('div');
+    this.div.className = 'myCopCar';
+    // this.div.style.backgroundColor = 'blue'; --Moved to CSS
+}
+CopCar.prototype.move = function() {
+    this.div = $('.myVehicle');
+    var ww = window.innerWidth;
+    this.div.animate({left: ww + 'px'}, 5000); //Same as Vehicle speed
+}
+
+// Child Classes Stop ---------------------------------------
 var vehicleArray = [];
 
+// Function Calls for Button types --------------------------
 function newVehicle() {
-    Vehicle.call(this);
+    //Vehicle.call(this);
     var vehicle = new Vehicle();
+    vehicle.insert();
+    vehicleArray.push(vehicle);
+}
+
+function newCar() {
+    //Vehicle.call(this);
+    var vehicle = new Car();
+    vehicle.insert();
+    vehicleArray.push(vehicle);
+}
+
+function newCopCar() {
+    //Vehicle.call(this);
+    var vehicle = new CopCar();
     vehicle.insert();
     vehicleArray.push(vehicle);
 }
@@ -50,23 +111,11 @@ function startGame() {
     }
 }
 
-
-// .myVehicle {
-//     width: 140px;
-//     height: 100px;
-//     background-color: red;
-//     font-family: Arial, Helvetica, sans-serif;
-//     color: #000;
-//     font-size: 30px;
-//     line-height: 60px;
-//     text-align: center;
-//     padding-top: 0px;
-//     margin: 4px;
-//     border: 1px solid #000;
-//     border-radius: 5px;
-//     -moz-border-radius: 5px;
-//     -webkit-border-radius: 5px;
-//     position: absolute;
-//     left: 0px;
-//     top: 0px;
-// }
+function reverse() {
+    if (vehicleArray[i] instanceof Car){
+            for (var i = 0; i < vehicleArray.length; i++) {
+            vehicleArray[i].moveback();
+        }
+    }
+}
+// END Function calls for all Button types-------------------
